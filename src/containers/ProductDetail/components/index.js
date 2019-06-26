e7041968c5c4aeb4e1ae2e35b9efd635fe6d813e
Adyasuren <38126041+Adyasuren@users.noreply.github.com>
2019-06-26 20:23:43 +0800

    UPDATE ProductDetail Magnify 2019.06.26

--- src/containers/ProductDetail/components/index.js
+++ src/containers/ProductDetail/components/index.js
@@ -1,3 +1,6 @@
 import Realational from "./Relational";
+import Magnifier from "./Magnifier";
+import Label from "./Label";
+import Gallery from "./Gallery";
 
-export { Realational };
+export { Realational, Magnifier, Label, Gallery };
--- src/containers/ProductDetail/list.js
+++ src/containers/ProductDetail/list.js
@@ -7,7 +7,7 @@ import {
   CARD_LIST_TYPES,
   CARD_NUMS_IN_ROW,
 } from "../../utils/Consts";
-import { Realational } from "./components";
+import { Realational, Gallery } from "./components";
 
 class Discount extends React.Component {
   renderRealational = () => {
@@ -19,9 +19,37 @@ class Discount extends React.Component {
       return console.log(error);
     }
   };
+
+  renderGallery = () => {
+    try {
+      const { detail } = this.props;
+      return (
+        <Gallery
+          images={detail.length === 0 ? [] : detail.images}
+          tags={detail.length === 0 ? [] : detail.products[0].tags}
+        />
+      );
+    } catch (error) {
+      return console.log(error);
+    }
+  };
+
   render() {
     return (
-      <div className="top-container">{/* this.renderRealational() */}</div>
+      <div className="section">
+        <div className="container">
+          <div className="product-detail-page col-md-12 col-sm-12 col-lg-12">
+            <div className="row row10">
+              <div className="col-sm-9 col-md-9 col-lg-9 row">
+                <div className="col-xl-5 col-lg-5 col-md-5">
+                  {this.renderGallery()}
+                </div>
+                {/* this.renderDetails() */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
