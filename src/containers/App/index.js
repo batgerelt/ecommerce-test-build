81f60e9b47861c537172bb74cfe1c76a666b0065
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2019-07-16 16:21:40 +0800

    UPDATE
    404 page Not found

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -9,6 +9,7 @@ import ScrollToTop from "react-router-scroll-top";
 import { Header, Footer, Mobilemenu } from "../../layouts";
 import { LoginModal } from "../../components/Login";
 import { RegistrationModal } from "../../components/Registration";
+import Notfound from "../Exception/404";
 import {
   Category as CategoryModel,
   Static as StaticModel,
@@ -71,7 +72,6 @@ class App extends Component {
     this.props.getStaticInfo();
     this.props.getMenu();
     this.props.getStaticPages();
-    // this.props.getNewProduct({});
   }
 
   render() {
@@ -107,6 +107,7 @@ class App extends Component {
               <Route path="/search/:id/:key" component={Search} />
               <Route path="/ResetPassword/:key" component={PassReset} />
               <Route path="/confirm/:key" component={Confirm} />
+              <Route path="*" component={Notfound} />
             </Switch>
 
             {/** fixed footer */}
