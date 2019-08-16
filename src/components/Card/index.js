d24e71d10cb16cf05d56b0921843132cc337225b
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2019-08-16 15:28:12 +0800

    UPDAET

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -321,7 +321,7 @@ class Card extends React.Component {
                   className={`col-md-6 ${list ? 'no-padding-l' : 'no-padding-r'} price-tag ${list ? 'price-tag-list' : ''}`}
                   style={{ textAlign: list ? 'center' : 'left' }}
                 >
-                  {item.pricetag}
+                  {lang === "mn" ? item.pricetag : item.pricetag_en === null ? item.pricetag : item.pricetag_en}
                 </div>
               )}
 
@@ -349,7 +349,10 @@ class Card extends React.Component {
 
               {/* elastic search price tag */}
               {item.pricetag === null ? null : (
-                <div className={`col-md-6 no-padding-r ${list ? 'list-price-tag' : ''}`} style={{ textAlign: "left" }} >
+                <div
+                  className={`col-md-6 ${list ? 'no-padding-l' : 'no-padding-r'} price-tag ${list ? 'price-tag-list' : ''}`}
+                  style={{ textAlign: list ? 'center' : 'left' }}
+                >
                   {lang === "mn" ? item.pricetag : item.pricetag_en === null ? item.pricetag : item.pricetag_en}
                 </div>
               )}
@@ -634,7 +637,7 @@ class Card extends React.Component {
           return (
             <div className="single-product list-product sale-product">
               <div className="image-container">
-                <Link to={item.route ? item.route : ""}>
+                <Link to={item.route ? item.route : `/productdetail/${item.skucd ? item.skucd : item.cd}`}>
                   <span
                     className="image"
                     style={{
@@ -646,10 +649,10 @@ class Card extends React.Component {
                 </Link>
               </div>
               <div className="info-container">
-                <Link to={item.route ? item.route : ""} className="name">
+                <Link to={item.route ? item.route : `/productdetail/${item.skucd ? item.skucd : item.cd}`} className="name">
                   <span>{itemName}</span>
                 </Link>
-                <Link to={item.route ? item.route : ""} className="cat">
+                <Link to={item.route ? item.route : `/productdetail/${item.skucd ? item.skucd : item.cd}`} className="cat">
                   <span>{featureText}</span>
                 </Link>
                 {
@@ -662,7 +665,7 @@ class Card extends React.Component {
                     /> : ""
                 }
                 <Link
-                  to={item.route ? item.route : ""}
+                  to={item.route ? item.route : `/productdetail/${item.skucd ? item.skucd : item.cd}`}
                   className="price"
                   style={{
                     padding: 0,
