6da670b3d184c4bec7bc84116161a548f7dfe682
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2019-08-21 09:20:17 +0800

    UPDATE

--- src/containers/Search/list.js
+++ src/containers/Search/list.js
@@ -351,8 +351,8 @@ class CategoryInfo extends React.Component {
         >
           {/* <Affix offsetTop={150} style={{ width: '100%' }} > */}
           <div
-            className={`left-panel-container ${
-              this.state.isMobilePanel ? " show" : null
+            className={`left-panel-container filter-sticky ${
+              this.state.isMobilePanel ? "show" : null
             }`}
             onClick={this.showMobilePanel}
           >
