d8379da5d4597ff94a279490a10fea37fb373d34
ariunkhuslen <38098562+ariunkhuslen@users.noreply.github.com>
2019-10-11 11:58:33 +0800

    checkout sign in

--- src/containers/Checkout/components/Signin/index.js
+++ src/containers/Checkout/components/Signin/index.js
@@ -5,6 +5,7 @@ import React from "react";
 import { injectIntl, FormattedMessage } from 'react-intl';
 import { Input, Form, Button, message } from "antd";
 import { Link } from "react-router-dom";
+import { FacebookLogin, GoogleLogin } from "../../../../components/Login/";
 
 class Signin extends React.Component {
   constructor(props) {
@@ -15,58 +16,77 @@ class Signin extends React.Component {
     };
   }
 
+  loginSocial = (param) => {
+    this.props.ouathLog({ body: { ...param } }).then(async (res) => {
+      console.log("result", res);
+      this.loggedData(res);
+      if (res.payload.success) {
+        this.setState({ confirm: this.state.direct });
+        await this.props.getUserInfo();
+        await this.props.getSystemLocation();
+      }
+    });
+  }
+
+  // eslint-disable-next-line consistent-return
+  loggedData = (r) => {
+    const { intl } = this.props;
+    if (r.payload.success) {
+      message.success(intl.formatMessage({ id: "loginModal.info.success" }));
+    } else {
+      if (r.payload.code) {
+        message.warning(intl.formatMessage({ id: r.payload.code }));
+      }
+      return null;
+    }
+    localStorage.setItem('img', r.payload.data[0].info.customerInfo.imgnm);
+    localStorage.setItem('auth', JSON.stringify(r.payload));
+    localStorage.setItem('percent', r.payload.data[0].info.customerInfo.cstatus);
+    localStorage.setItem('next', JSON.stringify(r.payload.data[0].info.customerInfo));
+    // eslint-disable-next-line consistent-return
+    this.props.getUserInfo().then(async (res) => {
+      if (res.payload.success) {
+        if (res.payload.data.main !== null) {
+          this.props.getDistrictLocation({ id: res.payload.data.main.provinceid });
+          this.props.getCommmitteLocation({ provid: res.payload.data.main.provinceid, distid: res.payload.data.main.districtid });
+        }
+        let { products } = this.props;
+        products = products.map(prod => ({
+          skucd: prod.skucd,
+          qty: prod.qty,
+        }));
+        let result = await this.props.increaseProductsByQtyRemotely({
+          iscart: 0,
+          body: products,
+        });
+        if (!result.payload.success) {
+          message.warning(intl.formatMessage({ id: result.payload.code }));
+        }
+        this.props.getProducts().then((res) => {
+          let k = res.payload.data.length - products.length;
+          if (res.payload.data.length !== 0 && k !== 0) {
+            this.props.history.push("/cart");
+          } else {
+            this.props.callback("2");
+          }
+        });
+      } else {
+        console.log(res.payload);
+      }
+    }).catch((err) => {
+      console.log('err: ', err);
+      // message.warning(intl.formatMessage({ id: result.payload.code }));
+    });
+    this.props.getSystemLocation({});
+  }
+
   onSubmitLogin = (e) => {
     e.preventDefault();
     const { intl } = this.props;
     this.props.form.validateFields(async (err, values) => {
       if (!err) {
-        this.props.login({ body: { ...values } }).then(async (r) => {
-          this.setState({ loading: true });
-          if (r.payload.success) {
-            localStorage.setItem('img', r.payload.data[0].info.customerInfo.imgnm);
-            localStorage.setItem('auth', JSON.stringify(r.payload));
-            localStorage.setItem('percent', r.payload.data[0].info.customerInfo.cstatus);
-            localStorage.setItem('next', JSON.stringify(r.payload.data[0].info.customerInfo));
-            // eslint-disable-next-line consistent-return
-            this.props.getUserInfo().then(async (res) => {
-              if (res.payload.success) {
-                if (res.payload.data.main !== null) {
-                  this.props.getDistrictLocation({ id: res.payload.data.main.provinceid });
-                  this.props.getCommmitteLocation({ provid: res.payload.data.main.provinceid, distid: res.payload.data.main.districtid });
-                }
-                let { products } = this.props;
-                products = products.map(prod => ({
-                  skucd: prod.skucd,
-                  qty: prod.qty,
-                }));
-                let result = await this.props.increaseProductsByQtyRemotely({
-                  iscart: 0,
-                  body: products,
-                });
-                if (!result.payload.success) {
-                  message.warning(intl.formatMessage({ id: result.payload.code }));
-                }
-                this.props.getProducts().then((res) => {
-                  let k = res.payload.data.length - products.length;
-                  if (res.payload.data.length !== 0 && k !== 0) {
-                    this.props.history.push("/cart");
-                  } else {
-                    this.props.callback("2");
-                  }
-                });
-              } else {
-                console.log(res.payload);
-              }
-            }).catch((err) => {
-              console.log('err: ', err);
-              // message.warning(intl.formatMessage({ id: result.payload.code }));
-            });
-            this.props.getSystemLocation({});
-          } else {
-            message.warning(intl.formatMessage({ id: r.payload.code }));
-          }
-          this.setState({ loading: false });
-        }).catch(err => console.log(err));
+        let result = await this.props.login({ body: { ...values } });
+        this.loggedData(result);
       } else {
         console.log(err);
       }
@@ -141,20 +161,10 @@ class Signin extends React.Component {
                   <FormattedMessage id="shared.form.label.forgotPassword" />
                 </Link>
               </label>
-              <button
-                type="submit"
-                className="btn btn-social btn-facebook"
-                style={{ width: "100%", marginTop: "10px" }}
-              >
-                <span><FormattedMessage id="shared.form.button.facebookLogin" /></span>
-              </button>
-              <button
-                type="submit"
-                className="btn btn-social btn-gmail"
-                style={{ width: "100%" }}
-              >
-                <span><FormattedMessage id="shared.form.button.googleLogin" /></span>
-              </button>
+              <div style={{ marginTop: "10px" }}>
+                <FacebookLogin {...this.prosps} {...this} />
+                <GoogleLogin {...this.props} {...this} />
+              </div>
             </div>
           </div>
         </Form>
