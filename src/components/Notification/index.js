1abf411b7f61cbf5c1b3b26a786356b87ed82216
ariunkhuslen <38098562+ariunkhuslen@users.noreply.github.com>
2019-10-23 17:24:04 +0800

    zaswar

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -8,24 +8,9 @@ export default class Notification extends React.Component {
     };
   }
 
-  /* onShow = () => {
-  } */
-
   render() {
     return (
-      <div
-        style={{
-          backgroundColor: "#444",
-          color: "white",
-          padding: "16px",
-          position: "absolute",
-          top: "16",
-          right: "16px",
-          zIndex: "999",
-          transition: "top 0.5s ease",
-        }}
-        className="d-none"
-      >
+      <div className="d-none">
         <span>Example text here</span>
       </div>
     );
