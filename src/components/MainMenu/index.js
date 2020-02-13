4172cfae8fc70e091ae7249e9203d3b2398600fb
ariunkhuslen <38098562+ariunkhuslen@users.noreply.github.com>
2020-02-13 18:38:55 +0800

    test

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -5,20 +5,36 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 
 class MainMenu extends React.Component {
+  handleClick = () => {
+    console.log("gagaga");
+  }
   renderMenus = () => {
     const data = this.props && this.props.dataSource;
     const { pathname, search } = this.props.history.location;
     let tmp = data.map((item, index) => {
+      console.log('item: ', item.link.includes('/e'));
       return (
         <Button key={index}>
           <li className="list-inline-item">
-            <Link to={item.link}>
-              <span style={{ color: pathname + search === item.link ? "#FFB81C" : "#FFFFFF" }}>
-                {this.props.intl.locale === "mn" ? item.menunm : item.menunm_en}
-              </span>
-            </Link>
+            {
+              item.link.includes('/e') ? (
+                <Link to={item.link} onClick={e => this.handleClick(e)}>
+                  <span style={{ color: pathname + search === item.link ? "#FFB81C" : "#FFFFFF" }}>
+                    {this.props.intl.locale === "mn" ? item.menunm : item.menunm_en}
+                  </span>
+                </Link>
+              )
+                :
+                (
+                  <Link to={item.link}>
+                    <span style={{ color: pathname + search === item.link ? "#FFB81C" : "#FFFFFF" }}>
+                      {this.props.intl.locale === "mn" ? item.menunm : item.menunm_en}
+                    </span>
+                  </Link>
+                )
+            }
           </li>
-        </Button>
+        </Button >
       );
     });
     return tmp;
