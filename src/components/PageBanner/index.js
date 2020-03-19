a0ff638cf1d70e64952a0d4264fce2bbe1fc61d0
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2020-03-19 16:18:19 +0800

    update

--- src/components/PageBanner/index.js
+++ src/components/PageBanner/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import { injectIntl } from 'react-intl';
+import PropTypes from "prop-types";
+import { injectIntl } from "react-intl";
 
 class Component extends React.Component {
   render() {
@@ -8,21 +8,41 @@ class Component extends React.Component {
       const { banner } = this.props;
       const { locale } = this.props.intl;
       if (banner) {
-        const imgnm = window.innerWidth < 576 ? (locale === "mn" ? banner[0].mobimgnm : banner[0].mobimgnm_en) : (locale === "mn" ? banner[0].imgnm : banner[0].imgnm_en ? banner[0].imgnm : banner[0].imgnm_en);
+        const imgnm =
+          window.innerWidth < 576
+            ? (locale === "mn"
+              ? banner[0].mobimgnm
+              : banner[0].mobimgnm_en)
+            : locale === "mn"
+              ? banner[0].imgnm
+              : banner[0].imgnm_en;
 
         return (
           <React.Fragment>
             <div className="page_header_banner_container d-flex">
-              <span className="page_header_banner_left" style={{ backgroundColor: banner[0].lcolor }} />
-              <div className={`container mx-auto`} onClick={() => banner[0].link && window.open(banner[0].link)}><img src={process.env.IMAGE + imgnm} alt="banner" /></div>
-              <span className="page_header_banner_rigth" style={{ backgroundColor: banner[0].rcolor }} />
+              <span
+                className="page_header_banner_left"
+                style={{ backgroundColor: banner[0].lcolor }}
+              />
+              <div
+                className={`container mx-auto`}
+                onClick={() => banner[0].link && window.open(banner[0].link)}
+              >
+                <img src={process.env.IMAGE + imgnm} alt="banner" />
+              </div>
+              <span
+                className="page_header_banner_rigth"
+                style={{ backgroundColor: banner[0].rcolor }}
+              />
             </div>
           </React.Fragment>
         );
       }
 
       return null;
-    } catch (error) { return null; }
+    } catch (error) {
+      return null;
+    }
   }
 }
 
