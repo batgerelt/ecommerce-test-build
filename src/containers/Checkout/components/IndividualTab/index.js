dc268348b66830f6e257ad137bf8f410791bad93
ariunkhuslen <38098562+ariunkhuslen@users.noreply.github.com>
2020-04-03 13:57:47 +0800

    Test

--- src/containers/Checkout/components/IndividualTab/index.js
+++ src/containers/Checkout/components/IndividualTab/index.js
@@ -149,6 +149,7 @@ class IndividualTab extends React.Component {
     if (password) {
       let cardno = mainState.cardInfo.cardno;
       this.props.checkEpointPin({ cardno, pincode: password }).then((res) => {
+        console.log(mainState);
         if (res.payload.success) {
           let chosenType = mainState.chosenDelivery;
           let totalPrice = mainState.totalPrice;
@@ -156,7 +157,6 @@ class IndividualTab extends React.Component {
           let usedPoint = 0;
           if (point > 0) {
             let deliveryPrice = chosenType.price;
-            console.log("epoint onoo: ", deliveryPrice);
             let tmp = mainState.cardInfo;
             if ((deliveryPrice + totalPrice) / 2 > tmp.point) {
               tmp.point = (parseFloat(tmp.point) - parseInt(point)).toFixed(2);
@@ -165,7 +165,7 @@ class IndividualTab extends React.Component {
               this.props.changeEpointUsedPoint(usedPoint);
             } else {
               tmp.point = (parseFloat(point) - parseFloat((deliveryPrice + totalPrice) / 2)).toFixed(2);
-              usedPoint = parseInt((deliveryPrice + totalPrice) / 2);
+              usedPoint = mainState.chosenDelivery.id !== 3 ? parseInt((deliveryPrice + totalPrice) / 2) : parseInt(totalPrice / 2);
               this.props.changeCardInfo(tmp);
               this.props.changeEpointUsedPoint(usedPoint);
             }
